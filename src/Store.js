42c39fa25302ff86225d525969d4901439dd9d53
Ken Church <30963709+kchurch9@users.noreply.github.com>
2020-02-14 18:30:10 +0300

    chat app creation

---
+++ src/Store.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import io from 'socket.io-client'
+
+export const CTX = React.createContext();
+
+const initState = {
+    Valentines: [
+
+    ],
+    Butt: [
+
+    ]
+}
+function reducer(state , action ) {
+    const {from,msg,topic}= action.payload;
+    switch (action.type){
+        case 'RECIEVE_MESSAGE':
+            return {
+                ...state,
+                [topic]: [
+                    ...state[topic],
+                    {from,msg}
+                ]
+            }
+        default:
+            return state
+    }
+}
+
+let socket;
+
+function sendChatAction(value){
+    socket.emit('chat message', value);
+}
+
+export default function Store(props){
+
+    const [allChats , dispatch] = React.useReducer(reducer, initState);
+
+    if (!socket) {
+        socket = io(':3003')
+        socket.on('chat message', function(msg){
+            dispatch({type: 'RECIEVE_MESSAGE', payload: msg});
+        });
+    }
+
+    const user = 'kaiden' + Math.random(100).toFixed(2)
+
+
+    return (
+        <CTX.Provider value={{allChats, sendChatAction, user}}>
+            {props.children}
+        </CTX.Provider>
+    )
+}
